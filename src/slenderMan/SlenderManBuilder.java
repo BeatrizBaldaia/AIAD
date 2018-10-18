58a517f59b03ca3dfbbb55c6107606a8898612cf
BeatrizBaldaia <bibibaldaia@hotmail.com>
2018-10-18 17:35:20 +0100

    first commit

---
+++ src/slenderMan/SlenderManBuilder.java
@@ -0,0 +1,66 @@
+package slenderMan;
+
+import slenderMan.Player;
+import slenderMan.Slender;
+import repast.simphony.context.Context;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactory;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactoryFinder;
+import repast.simphony.context.space.graph.NetworkBuilder;
+import repast.simphony.context.space.grid.GridFactory;
+import repast.simphony.context.space.grid.GridFactoryFinder;
+import repast.simphony.dataLoader.ContextBuilder;
+import repast.simphony.engine.environment.RunEnvironment;
+import repast.simphony.parameter.Parameters;
+import repast.simphony.random.RandomHelper;
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.continuous.NdPoint;
+import repast.simphony.space.continuous.RandomCartesianAdder;
+import repast.simphony.space.grid.Grid;
+import repast.simphony.space.grid.GridBuilderParameters;
+import repast.simphony.space.grid.SimpleGridAdder;
+import repast.simphony.space.grid.WrapAroundBorders;
+
+public class SlenderManBuilder implements ContextBuilder<Object> {
+
+	@Override
+	public Context build(Context<Object> context) {
+		context.setId("SlenderMan");
+
+		NetworkBuilder<Object> netBuilder = new NetworkBuilder<Object>(
+				"infection network", context, true);
+		netBuilder.buildNetwork();
+
+		ContinuousSpaceFactory spaceFactory = ContinuousSpaceFactoryFinder
+				.createContinuousSpaceFactory(null);
+		ContinuousSpace<Object> space = spaceFactory.createContinuousSpace(
+				"space", context, new RandomCartesianAdder<Object>(),
+				new repast.simphony.space.continuous.WrapAroundBorders(), 50,
+				50);
+
+		GridFactory gridFactory = GridFactoryFinder.createGridFactory(null);
+		Grid<Object> grid = gridFactory.createGrid("grid", context,
+				new GridBuilderParameters<Object>(new WrapAroundBorders(),
+						new SimpleGridAdder<Object>(), true, 50, 50));
+
+		Parameters params = RunEnvironment.getInstance().getParameters();
+		context.add(new Slender(space, grid));
+
+		int player_count = (Integer) params.getValue("player_count");
+		for (int i = 0; i < player_count; i++) {
+			int energy = RandomHelper.nextIntFromTo(4, 10);
+			context.add(new Player(space, grid, energy));
+		}
+
+		for (Object obj : context) {
+			NdPoint pt = space.getLocation(obj);
+			grid.moveTo(obj, (int) pt.getX(), (int) pt.getY());
+		}
+		
+		if (RunEnvironment.getInstance().isBatch()) {
+			RunEnvironment.getInstance().endAt(20);
+		}
+
+		return context;
+	}
+
+}
